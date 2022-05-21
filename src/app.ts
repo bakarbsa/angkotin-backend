97d8819d0c899b4b144719e17827093bdb20ceb3
bakarbsa <abubakar.bsa30@gmail.com>
2022-05-21 08:24:56 +0700

    add package and app routing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express, { Express, json } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import UserRouter from './routes/UserRouter'
+import { AuthController } from './controllers/AuthController';
+import { Authentication, IsAdmin, IsDriver, IsPassenger } from './middleware';
+import passengerRouter from './routes/PassengerRouter';
+import driverRouter from './routes/DriverRouter';
+import adminRouter from './routes/AdminRouter';
+import registerRouter from './routes/RegisterRouter';
 
 dotenv.config();
 
@@ -10,6 +15,11 @@ const app: Express = express();
 app.use(cors());
 app.use(json());
 
-app.use('/users', UserRouter);
+app.post('/auth', AuthController);
+
+app.use('/register', registerRouter);
+app.use('/passenger', Authentication, IsPassenger, passengerRouter);
+app.use('/driver', Authentication, IsDriver, driverRouter);
+app.use('/admin', Authentication, IsAdmin, adminRouter);
 
 export default app;
