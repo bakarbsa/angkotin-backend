aec5c9f595cfe7fb32fd2706a930f1b876c2dbac
bakarbsa <abubakar.bsa30@gmail.com>
2022-05-26 07:27:53 +0700

    change https to http

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,5 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`[server] Server is running at https://localhost:${PORT}`);
+  console.log(`[server] Server is running at http://localhost:${PORT}`);
 });
