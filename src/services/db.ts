256fa2f318145c018804a8f30c39fe2bdf8ae07a
bakarbsa <abubakar.bsa30@gmail.com>
2022-06-11 09:13:13 +0700

    update cred firebase

--- src/services/db.ts
+++ src/services/db.ts
@@ -2,13 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxwtbD4_jgGk8dQ9irYqcJ5IdYkNdeij0",
-  authDomain: "testing-41397.firebaseapp.com",
-  databaseURL: "https://testing-41397-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "testing-41397",
-  storageBucket: "testing-41397.appspot.com",
-  messagingSenderId: "285672254334",
-  appId: "1:285672254334:web:15c291f71b84107e68a12f"
+  apiKey: "AIzaSyA0X7rRk0gmQpAC2lHsYjyPmzEB-3nKjCA",
+  authDomain: "angkotin-352405.firebaseapp.com",
+  projectId: "angkotin-352405",
+  storageBucket: "angkotin-352405.appspot.com",
+  messagingSenderId: "677320649420",
+  appId: "1:677320649420:web:56c3f7639e4d2aab733069"
 };
 
 const app = initializeApp(firebaseConfig);
